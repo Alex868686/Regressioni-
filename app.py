e89f1e68089ff90f3eba12459c6188f0324991ce
Alex868686 <barbaro222alessandro@gmail.com>
2023-05-13 17:45:58 +0200

    test

---
+++ app.py
@@ -0,0 +1,10 @@
+def main():
+    import matplotlib.pyplot as plt
+    from sklearn.datasets import load_iris
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.model_selection import train_test_split
+    
+if __name__ == "__main__":
+    main()
+    
+    
